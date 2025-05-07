8b49d6ac868ab4407e6010bc095b54e0716c4a87
Ayan-Yue Gupta <43908663+ayanyuegupta@users.noreply.github.com>
2025-05-07 20:40:04 +0000

    testing

--- first_script.py
+++ first_script.py
@@ -1,1 +1,25 @@
-print('hello')
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+api_key = 'AIzaSyDcRnH0wEv2zUWqrPkdOaHmMkNKLb6x0ic'
+
+client = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
+request = client.search().list(
+    part='snippet',
+    maxResults=10,
+    q='covid',
+    relevanceLanguage='en'
+)
+
+print(request.execute())
+
+
+
+
+
+
+
+
+
+
